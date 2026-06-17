37e87c60bb499390c83607bf2a5fa8dfcdf96199
Selvabharath <g.selvabharat@gmail.com>
2026-06-17 20:06:05 +0800

    fix: simplify config file format to bypass node import error

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vitepress'
-export default defineConfig({
+export default {
   title: "Automated Research Blog",
   description: "AI/ML Latest Insights",
   base: '/automated_blog_v1/', 
   themeConfig: {
-    nav: [{ text: 'Home', link: '/' }],
-    sidebar: [{ text: 'Latest Research Posts', items: [{ text: 'Welcome to My Blog', link: '/' }] }]
+    nav: [
+      { text: 'Home', link: '/' }
+    ],
+    sidebar: [
+      {
+        text: 'Latest Research Posts',
+        items: [
+          { text: 'Welcome to My Blog', link: '/' }
+        ]
+      }
+    ]
   }
-})
+}
